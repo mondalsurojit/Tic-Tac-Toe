8c47ab82f7348c93f5addca63b06fc4875ea490c
surojitmondal <gobindomondal539@gmail.com>
2023-12-12 15:46:22 +0530

    added victoryCheck and afterVictory functions in script.js file

--- script.js
+++ script.js
@@ -2,6 +2,34 @@ let cells = Array.from(document.querySelectorAll(".cells"));
 let count = 0;
 let twoDArray = [];
 
+function victoryCheck() {
+    // Implement the logic to check for a winning condition
+    for (let i = 0; i < 3; i++) {
+        if (twoDArray[i][0].innerHTML == twoDArray[i][1].innerHTML && twoDArray[i][1].innerHTML == twoDArray[i][2].innerHTML && twoDArray[i][0].innerHTML != "") {
+            console.log("horizonal win");
+            afterVictory();
+        }
+    }
+    for (let j = 0; j < 3; j++) {
+        if (twoDArray[0][j].innerHTML == twoDArray[1][j].innerHTML && twoDArray[1][j].innerHTML == twoDArray[2][j].innerHTML && twoDArray[0][j].innerHTML != "") {
+            console.log("vertical win");
+            afterVictory();
+        }
+    }
+    if ((twoDArray[0][0].innerHTML == twoDArray[1][1].innerHTML && twoDArray[1][1].innerHTML == twoDArray[2][2].innerHTML && twoDArray[1][1].innerHTML != "") || (twoDArray[0][2].innerHTML == twoDArray[1][1].innerHTML && twoDArray[1][1].innerHTML == twoDArray[2][0].innerHTML && twoDArray[1][1].innerHTML != "")) {
+        console.log("diagonal win");
+        afterVictory();
+    }
+}
+
+function afterVictory() {
+    twoDArray.forEach((twoDArrayCells) => {
+        twoDArrayCells.forEach((twoDArrayCellsCells) => {
+            twoDArrayCellsCells.removeEventListener("click", clickEvent);
+        });
+    });
+}
+
 function defaultState() {
     for (let i = 0; i < 3; i++) {
         twoDArray[i] = [];
@@ -16,40 +44,23 @@ function clickEvent() {
     if (count % 2 == 0) {
         this.innerHTML = "❌";
         this.removeEventListener("click", clickEvent);
-        console.log(twoDArray);
     } else {
         this.innerHTML = "⭕";
         this.removeEventListener("click", clickEvent);
     }
     count++;
     // Call victory check here if needed
+    victoryCheck();
 }
 
 function resetGame() {
-    for (let i = 0; i < 3; i++) {
-        for (let j = 0; j < 3; j++) {
-            twoDArray[i][j].innerHTML = "";
-            twoDArray[i][j].addEventListener("click", clickEvent);
-        }
-    }
+    twoDArray.forEach((twoDArrayCells) => {
+        twoDArrayCells.forEach((twoDArrayCellsCells) => {
+            twoDArrayCellsCells.innerHTML = "";
+            twoDArrayCellsCells.addEventListener("click", clickEvent);
+        });
+    });
     count = 0;
-}
-
-function victoryCheck() {
-    // Implement the logic to check for a winning condition
-    for (let i = 0; i < 3; i++) {
-        for (let j = 0; j < 3; j++) {
-            if (twoDArray[i][j] == twoDArray[i][j + 1] == twoDArray[i][j + 2]) {
-                console.log("win");
-            }
-            else if (twoDArray[i][j] == twoDArray[i + 1][j] == twoDArray[i + 2][j]) {
-                console.log("win");
-            }
-            else if (twoDArray[0][0] == twoDArray[1][1] == twoDArray[2][2]) {
-                console.log("win");
-            }
-        }
-    }
 }
 
 // Call initializeGame to set up the initial state
